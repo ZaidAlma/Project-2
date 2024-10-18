e7659f2a8703aa0f923fc6deb4a31fcba1d374c7
Heyun Kim <heyunkim06@gmail.com>
2024-10-17 22:22:05 -0400

    Manager can read file

--- Project2/src/Technician.java
+++ Project2/src/Technician.java
@@ -12,6 +12,32 @@ public class Technician extends Provider{
         return ratePerVisit;
     }
 
+    @Override
+    public String toString(){
+        Location location = super.getLocation();
+        String locationInfo ="";
+
+        if(location == Location.BRIDGEWATER){
+            locationInfo = "BRIDGEWATER, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        else if(location == Location.EDISON){
+            locationInfo = "EDISON, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        else if(location == Location.PISCATAWAY){
+            locationInfo = "PISCATAWAY, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        else if(location == Location.PRINCETON){
+            locationInfo = "PRINCETON, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        else if(location == Location.MORRISTOWN){
+            locationInfo = "MORRISTOWN, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        else if(location == Location.CLARK){
+            locationInfo = "CLARK, " + super.getLocation().getCounty() + " " + super.getLocation().getZipCode();
+        }
+        return "[" +profile.getFirstName() + " " + profile.getLastName() + " " + profile.getDob() + ", " + locationInfo + "] [rate: " + ratePerVisit + "]";
+    }
+
 //    public int getRatePerVisit(){
 //        return ratePerVisit;
 //    }
